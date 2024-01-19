5426da2ddce620ac66b5c9d13014b9549b3eeb4c
Thepython555 <144301348+thepython555@users.noreply.github.com>
2024-01-18 20:56:51 -0500

    that was easy

--- api/index.py
+++ api/index.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect
+import random
+import requests
 
 app = Flask(__name__)
 
+def getRandomProject():
+    #really messy. sorry.
+    try:
+        running = True
+        notfoundtext = requests.get("https://api.scratch.mit.edu/projects/7676767676767676").text
+        projectcount = 908838871
+        while running:
+            idrandom = random.randint(0,projectcount)
+            urlrandom = f"https://api.scratch.mit.edu/projects/{idrandom}"
+            if requests.get(urlrandom).text != notfoundtext:
+                running = False
+                return str(idrandom)
+    except Exception:
+        return 0
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return redirect(f"https://scratch.mit.edu/projects/{getRandomProject()}", code=200)
 
-@app.route('/about')
+@app.route('/api')
 def about():
-    return 'About'
+    return getRandomProject()
